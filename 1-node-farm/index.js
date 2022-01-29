cc200ba68193629f966ff78d8765ebe2dab97c58
shaydeecoder <shaydeecoder@gmail.com>
2022-01-29 20:54:28 +0100

    Reading & Writing files in a blocking and non-blocking way

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,14 +1,32 @@
 // Require File System Module
 const fs = require("fs");
 
-const hello = "Hello World";
-// console.log(hello);
+/////////////////////////////////////////
+// FILES
 
-// Reading files
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// Reading files synchronously
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-// Writing files
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written!");
+// Writing files synchronously
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+// Reading files asynchronously
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR! 💥");
+
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written!");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file!");

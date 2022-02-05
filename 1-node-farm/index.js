ba47094e3555015620b3d155327f5b398c4089de
shaydeecoder <shaydeecoder@gmail.com>
2022-02-05 07:57:01 +0100

    Refactored index.js to read file synchronously & edited product.html with placeholder data

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -36,6 +36,9 @@ const url = require("url");
 /////////////////////////////////////////
 // SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 // Create a server
 const server = http.createServer((req, res) => {
   // Send response to client-side
@@ -48,6 +51,16 @@ const server = http.createServer((req, res) => {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    /* fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+
+      res.writeHead(200, { "Content-type": "application/json" });
+      res.end(data);
+    }); */
+
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",

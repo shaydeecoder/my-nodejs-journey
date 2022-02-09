1609e57777db12a1366289546b58a650f72f086e
Shaydee Coder <shaydeecoder@gmail.com>
2022-02-09 09:18:47 +0100

    Changed threadpool size

--- 2-how-node-works/event-loop.js
+++ 2-how-node-works/event-loop.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
+
+// Changing threadpool size
+process.env.UV_THREADPOOL_SIZE = 4;
 
 setTimeout(() => console.log('Time 1 finished'), 0);
 setImmediate(() => console.log('Immediate 1 finished'));
@@ -12,6 +18,22 @@ fs.readFile('test-file.txt', () => {
   setImmediate(() => console.log('Immediate 2 finished'));
 
   process.nextTick(() => console.log('Process.nextTick'));
+
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password encrypted');
+  });
 });
 
 console.log('Hello from the top-level code');

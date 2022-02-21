55d23a1d8ac248e071d2f106209b2f1d69595e2b
Shaydee Coder <shaydeecoder@gmail.com>
2022-02-22 00:42:32 +0100

    module in practice - module.exports

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -1,2 +1,7 @@
-console.log(arguments);
-console.log(require('module').wrapper);
+// console.log(arguments);
+// console.log(require('module').wrapper);
+
+// module.exports
+const C = require('./test-file-1');
+const calc1 = new C();
+console.log(calc1.add(2, 5));
---
+++ 2-how-node-works/test-file-1.js
@@ -0,0 +1,31 @@
+// Example 1
+/* class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  divide(a, b) {
+    return a / b;
+  }
+}
+
+module.exports = Calculator; */
+
+// Example 2
+module.exports = class {
+  add(a, b) {
+    return a + b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  divide(a, b) {
+    return a / b;
+  }
+};

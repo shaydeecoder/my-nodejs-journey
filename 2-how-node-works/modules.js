354a37701d5657da2986ac1545afcc3df4ae538e
Shaydee Coder <shaydeecoder@gmail.com>
2022-02-22 00:45:39 +0100

    Renamed text-file-1 to text-module-1

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -2,6 +2,6 @@
 // console.log(require('module').wrapper);
 
 // module.exports
-const C = require('./test-file-1');
+const C = require('./test-module-1');
 const calc1 = new C();
 console.log(calc1.add(2, 5));

e1fea30ce89b34cebfc6cbb36d9646983aaff1fc
shaydeecoder <shaydeecoder@gmail.com>
2022-02-22 23:51:25 +0100

    modules in practice - exports shorthand

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -5,3 +5,7 @@
 const C = require('./test-module-1');
 const calc1 = new C();
 console.log(calc1.add(2, 5));
+
+// exports
+const calc2 = require('./text-module-2');
+console.log(calc2.multiple(2, 5));

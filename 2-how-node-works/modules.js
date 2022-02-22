b27f647dc77c755c2a27d2f23e4acaf5d95d33c1
shaydeecoder <shaydeecoder@gmail.com>
2022-02-23 00:18:03 +0100

    Renamed text-module-2 to test-module-2, modules in practice - caching, & added asynchronous js starter file

--- 2-how-node-works/modules.js
+++ 2-how-node-works/modules.js
@@ -7,9 +7,14 @@ const calc1 = new C();
 console.log(calc1.add(2, 5));
 
 // exports
-const calc2 = require('./text-module-2');
+const calc2 = require('./test-module-2');
 console.log(calc2.multiply(2, 5));
 
 // Destructuring exports
-const { multiply } = require('./text-module-2');
+const { multiply } = require('./test-module-2');
 console.log(multiply(2, 5));
+
+// Caching
+require('./test-module-3')();
+require('./test-module-3')();
+require('./test-module-3')();

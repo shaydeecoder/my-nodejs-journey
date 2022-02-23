c3b04954c667f5f63f663717d33d08c83e25c5d4
Shaydee Coder <shaydeecoder@gmail.com>
2022-02-23 07:26:07 +0100

    From callback hell to promises

--- 3-asynchronous-js/index.js
+++ 3-asynchronous-js/index.js
@@ -4,7 +4,7 @@ const superagent = require('superagent');
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
-  superagent
+  /* superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
       if (err) return console.log(err.message);
@@ -14,5 +14,19 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
         if (err) return console.log(err.message);
         console.log('Random dog image saved to file!');
       });
+    }); */
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((res) => {
+      console.log(res.body.message);
+
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log('Random dog image saved to file!');
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });

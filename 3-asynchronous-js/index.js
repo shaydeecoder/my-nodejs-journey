eb398175a6d87ec295760d03efd1807d6ec3661a
Shaydee Coder <shaydeecoder@gmail.com>
2022-02-23 08:23:48 +0100

    Consumed writeFilePro promise

--- 3-asynchronous-js/index.js
+++ 3-asynchronous-js/index.js
@@ -30,11 +30,10 @@ readFilePro(`${__dirname}/dog.txt`)
   })
   .then((res) => {
     console.log(res.body.message);
-
-    fs.writeFile('dog-img.txt', res.body.message, (err) => {
-      if (err) return console.log(err.message);
-      console.log('Random dog image saved to file!');
-    });
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image saved to file!');
   })
   .catch((err) => {
     console.log(err.message);

9bed20f5acaf5a6cef2e54addc329c8e30829688
Shaydee Coder <shaydeecoder@gmail.com>
2022-02-23 07:42:42 +0100

    Building a readFile promise

--- 3-asynchronous-js/index.js
+++ 3-asynchronous-js/index.js
@@ -1,20 +1,30 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+// Promisifying readFile
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject(`Couldn't find that file 🙁`);
+      resolve(data);
+    });
+  });
+};
+
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
-  /* superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-      console.log(res.body.message);
+  // superagent
+  //   .get(`https://dog.ceo/api/breed/${data}/images/random`)
+  //   .end((err, res) => {
+  //     if (err) return console.log(err.message);
+  //     console.log(res.body.message);
 
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        if (err) return console.log(err.message);
-        console.log('Random dog image saved to file!');
-      });
-    }); */
+  //     fs.writeFile('dog-img.txt', res.body.message, (err) => {
+  //       if (err) return console.log(err.message);
+  //       console.log('Random dog image saved to file!');
+  //     });
+  //   });
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)

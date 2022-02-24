d2362f3c66089125c97ae1cdf5d07234019a1394
shaydeecoder <shaydeecoder@gmail.com>
2022-02-24 08:29:50 +0100

    Starting a server & sending a response to the client - express

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,3 +1,14 @@
 const express = require('express');
 
 const app = express();
+
+// Sending a response to the client
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from the server side!');
+});
+
+// Starting a server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});

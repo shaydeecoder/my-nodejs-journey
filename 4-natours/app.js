25ec579ffee6d1486a69beeab6006819acaf6f30
Shaydee Coder <shaydeecoder@gmail.com>
2022-02-24 08:31:58 +0100

    Sending json to the client - express

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,8 +3,15 @@ const express = require('express');
 const app = express();
 
 // Sending a response to the client
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res.status(200).send('Hello from the server side!');
+}); */
+
+// Sending json to client
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
 });
 
 // Starting a server

cd363a010ed35fc37e1b28822630fc8852b6657f
shaydeecoder <shaydeecoder@gmail.com>
2022-05-16 07:17:12 +0100

    Added basic routing using switch statement

--- 5-Blog/server.js
+++ 5-Blog/server.js
@@ -7,8 +7,22 @@ const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader('Content-Type', 'text/html');
 
+  // basic routing
+  let path = './views/';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      break;
+    case 'about':
+      path += 'about.html';
+      break;
+    default:
+      path += '404.html';
+      break;
+  }
+
   // send an html file
-  fs.readFile('./views/index.html', (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();

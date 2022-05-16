a0604ce639f01b1a2b58f44959d6e7beda0ef91d
shaydeecoder <shaydeecoder@gmail.com>
2022-05-16 06:49:51 +0100

    Renamed '5. Blog' to '5-Blog'

--- 5-Blog/server.js
+++ 5-Blog/server.js
@@ -1,10 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   // set header content type
   res.setHeader('Content-Type', 'text/html');
+
+  // send an html file
+  fs.readFile('./views/index.html', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, 'localhost', () => {

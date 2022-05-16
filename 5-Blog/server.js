ca5f2fe30c5872ad21d7e0a022cbce0473acbdf9
Shaydee Coder <shaydeecoder@gmail.com>
2022-05-16 07:24:11 +0100

    Added status code to routes

--- 5-Blog/server.js
+++ 5-Blog/server.js
@@ -12,12 +12,15 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case '/':
       path += 'index.html';
+      res.statusCode = 200;
       break;
     case 'about':
       path += 'about.html';
+      res.statusCode = 200;
       break;
     default:
       path += '404.html';
+      res.statusCode = 404;
       break;
   }
 

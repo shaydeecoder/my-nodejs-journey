d37111de9e8f5f0197101a42ce825cf4393fe95b
shaydeecoder <shaydeecoder@gmail.com>
2022-05-18 07:37:05 +0100

    Added routing with express

--- 5-Blog/app.js
+++ 5-Blog/app.js
@@ -7,5 +7,11 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.send('<p>home page</p>');
+  // res.send('<p>home page</p>');
+  res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+  // res.send('<p>home page</p>');
+  res.sendFile('./views/about.html', { root: __dirname });
 });

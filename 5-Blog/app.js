503b2e1e4a02ca6515e5c9b55d085bcf41ed359e
Shaydee Coder <shaydeecoder@gmail.com>
2022-05-18 08:25:41 +0100

    Added status code 404 using express

--- 5-Blog/app.js
+++ 5-Blog/app.js
@@ -24,5 +24,5 @@ app.get('/about-us', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-  res.sendFile('./views/404.html', { root: __dirname });
+  res.status(404).sendFile('./views/404.html', { root: __dirname });
 });

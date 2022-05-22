914e8dabf650f05c5f96c7515deef5d5f55f2910
Shaydee Coder <shaydeecoder@gmail.com>
2022-05-22 13:58:14 +0100

    Added 404.ejs

--- 5-Blog/app.js
+++ 5-Blog/app.js
@@ -31,5 +31,5 @@ app.get('/about-us', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404');
 });

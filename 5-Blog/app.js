e8a8437dc64dfabdeeeb8ca268875c96cceb2b35
shaydeecoder <shaydeecoder@gmail.com>
2022-06-24 09:47:23 +0100

    Sent document from db to view

--- 5-Blog/app.js
+++ 5-Blog/app.js
@@ -76,11 +76,14 @@ app.get('/single-blog', (req, res) => {
 });
 
 // routing with express
+// basic routes
 app.get('/', (req, res) => {
   // res.send('<p>home page</p>');
   // res.sendFile('./views/index.html', { root: __dirname });
 
-  const blogs = [
+  res.redirect('/blogs');
+
+  /* const blogs = [
     {
       title: 'Yoshi finds eggs',
       snippet: 'Lorem ipsum dolor sit amet consectetur',
@@ -96,11 +99,23 @@ app.get('/', (req, res) => {
   ];
 
   // render view
-  res.render('index', { title: 'Home', blogs });
+  res.render('index', { title: 'Home', blogs }); */
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'All Blogs', blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get('/blogs/create', (req, res) => {

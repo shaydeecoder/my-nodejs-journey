20aa56c6bc49ee423e563bfafa39fefefdb6ec37
Shaydee Coder <shaydeecoder@gmail.com>
2022-06-28 08:32:58 +0100

    Added delete request

--- 5-Blog/app.js
+++ 5-Blog/app.js
@@ -147,6 +147,18 @@ app.get('/blogs/:id', (req, res) => {
     });
 });
 
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });

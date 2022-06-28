c4e4ac3531af42c5b10fcb32f54ac22e01583d81
Shaydee Coder <shaydeecoder@gmail.com>
2022-06-28 09:17:32 +0100

    Scoped blogRoutes to '/blog'

--- 5-Blog/routes/blogRoutes.js
+++ 5-Blog/routes/blogRoutes.js
@@ -3,7 +3,7 @@ const Blog = require('../models/blog');
 
 const router = express.Router();
 
-router.get('/blogs', (req, res) => {
+router.get('/', (req, res) => {
   Blog.find()
     .sort({ createdAt: -1 })
     .then((result) => {
@@ -14,7 +14,7 @@ router.get('/blogs', (req, res) => {
     });
 });
 
-router.post('/blogs', (req, res) => {
+router.post('/', (req, res) => {
   // console.log(req.body);
   const blog = new Blog(req.body);
 
@@ -28,11 +28,11 @@ router.post('/blogs', (req, res) => {
     });
 });
 
-router.get('/blogs/create', (req, res) => {
+router.get('/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
 
-router.get('/blogs/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const id = req.params.id;
   console.log(id);
 
@@ -45,7 +45,7 @@ router.get('/blogs/:id', (req, res) => {
     });
 });
 
-router.delete('/blogs/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   const id = req.params.id;
 
   Blog.findByIdAndDelete(id)

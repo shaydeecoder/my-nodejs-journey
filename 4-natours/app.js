6bf633f26a713276ea8f804fe0ec49fca95b0cff
Shaydee Coder <shaydeecoder@ShaydeeCoders-MacBook-Pro.local>
2022-10-07 08:18:38 +0100

    Added routes and route handlers

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
 const express = require('express');
+const { fail } = require('assert');
 
 const app = express();
+
+app.use(express.json());
 
 // Sending a response to the client
 /* app.get('/', (req, res) => {
@@ -8,14 +12,102 @@ const app = express();
 }); */
 
 // Sending json to client
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res
     .status(200)
     .json({ message: 'Hello from the server side!', app: 'Natours' });
+}); */
+
+/* app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+}); */
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
 
 // Starting a server

f3dc16a82182c195f0f018f699d96d79133f95a7
Shaydee Coder <shaydeecoder@gmail.com>
2022-10-13 08:12:58 +0100

    Worked on createTour controller

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -39,21 +39,34 @@ exports.getTour = (req, res) => {
   // const id = req.params.id * 1;
   // const tour = tours.find((el) => el.id === id);
 
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour,
-  //   },
-  // });
+  res.status(200).json({
+    status: 'success',
+    // data: {
+    //   tour,
+    // },
+  });
 };
 
-exports.createTour = (req, res) => {
-  // res.status(201).json({
-  //   status: 'success',
-  //   data: {
-  //     tour: newTour,
-  //   },
-  // });
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({});
+    // newTour.save();
+
+    // Another way of creating a document
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {

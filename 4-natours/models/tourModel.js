c7becbd2679599d2e0262819dce5d165731ecf78
Shaydee Coder <shaydeecoder@gmail.com>
2022-10-13 05:36:16 +0100

    Added tourModel file

---
+++ 4-natours/models/tourModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// Creating schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// Creating model
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -22,43 +22,6 @@ mongoose
     console.log('DB connection successful!');
   });
 
-// Creating schema
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-// Creating model
-const Tour = mongoose.model('Tour', tourSchema);
-
-// Creating a new tour with the model
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
-  price: 497,
-});
-
-// Creating a document with the model
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR 💥:', err);
-  });
-
 // Starting a server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {

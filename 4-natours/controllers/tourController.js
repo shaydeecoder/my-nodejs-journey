f5b2c2a6e114d6d6fcfa4f9aaee7cc50b6e45d16
shaydeecoder <shaydeecoder@gmail.com>
2022-10-16 13:01:22 +0100

    Aggregation pipeline - unwinding & projecting

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -162,7 +162,60 @@ exports.getTourStats = async (req, res) => {
 
     res.status(200).json({
       status: 'success',
+      results: stats.length,
       data: stats,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      results: plan.length,
+      data: plan,
     });
   } catch (err) {
     res.status(404).json({

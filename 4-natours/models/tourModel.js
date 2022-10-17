e25a1d1bfcf7df729fac3e0f79dfc74b857d143e
shaydeecoder <shaydeecoder@gmail.com>
2022-10-17 07:22:57 +0100

    Added document middleware

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // Creating schema
 const tourSchema = new mongoose.Schema(
@@ -9,6 +10,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -65,6 +67,23 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+/* tourSchema.pre('save', function (next) {
+  console.log('Will save document...');
+  next();
+});
+
+// runs after .save() and .create()
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next();
+}); */
 
 // Creating model
 const Tour = mongoose.model('Tour', tourSchema);

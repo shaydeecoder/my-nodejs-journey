88120ab520d3bae3772fbbfb0d669c6c59b38489
shaydeecoder <shaydeecoder@gmail.com>
2022-10-18 06:59:34 +0100

    Aggregation middleware

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -155,9 +155,9 @@ exports.getTourStats = async (req, res) => {
       {
         $sort: { avgPrice: 1 },
       },
-      {
-        $match: { _id: { $ne: 'EASY' } },
-      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
     ]);
 
     res.status(200).json({
--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -100,7 +100,15 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
-  console.log(docs);
+  // console.log(docs);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE: runs before/after an aggregation happens
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
   next();
 });
 

ac26b21c7ea2ef5f6b132e759d7b53781922cde3
shaydeecoder <shaydeecoder@gmail.com>
2022-12-18 07:18:24 +0100

    Error Handling: Handling unhandled routes

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -16,11 +16,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -68,5 +63,13 @@ app
 // Mounting multiple routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handling unhandled routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const validator = require('validator');
+// const validator = require('validator');
 
 // Creating schema
 const tourSchema = new mongoose.Schema(

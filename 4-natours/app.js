6b38c079be0a8f91fa4ae70d8a2df2bd31fd1da0
shaydeecoder <shaydeecoder@gmail.com>
2022-12-18 08:23:36 +0100

    Refactored the error handling middleware into errorController

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -82,14 +83,6 @@ app.all('*', (req, res, next) => {
 });
 
 // Error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;

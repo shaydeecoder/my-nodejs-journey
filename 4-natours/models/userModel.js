894c1e4ee6adcd14bed8e9c3d15d96aaaa60e1a4
shaydeecoder <shaydeecoder@gmail.com>
2022-12-21 08:33:59 +0100

    feat: Implement user model

---
+++ 4-natours/models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    trim: true,
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'A user must confirm their password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

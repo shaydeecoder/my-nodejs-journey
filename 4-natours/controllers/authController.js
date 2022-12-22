804fc6749d7cab7470c2e9e077d2657bf03f95c2
shaydeecoder <shaydeecoder@gmail.com>
2022-12-22 06:51:08 +0100

    feat: Implement JWT on sign up

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -10,8 +10,13 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },

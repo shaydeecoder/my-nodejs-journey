0d62d4ccd568c687b64e123751d290162762545c
shaydeecoder <shaydeecoder@gmail.com>
2022-12-28 11:24:58 +0100

    feat: Implement reset token & sendEmail

--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -1,6 +1,8 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const { resetPassword } = require('../controllers/authController');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -40,6 +42,8 @@ const userSchema = new mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -73,6 +77,21 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
   // FALSE means NOT changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);

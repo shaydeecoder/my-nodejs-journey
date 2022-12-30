4014cca25cb8c07fa2f67c629f2a234e36952658
shaydeecoder <shaydeecoder@gmail.com>
2022-12-30 08:59:05 +0100

    feat: Update current user data

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -1,5 +1,16 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
 
 // Users route handlers
 exports.getAllUsers = catchAsync(async (req, res, next) => {
@@ -11,6 +22,34 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: users.length,
     data: {
       users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password udates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  // 2) Filter out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -17,6 +17,8 @@ router.patch(
   authController.updatePassword
 );
 
+router.patch('/updateMe', authController.protect, userController.updateMe);
+
 router
   .route('/')
   .get(userController.getAllUsers)

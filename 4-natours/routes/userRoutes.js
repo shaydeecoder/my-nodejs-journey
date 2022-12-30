49b6f031bbc902ac4308bb4c252ccdb36815b186
shaydeecoder <shaydeecoder@gmail.com>
2022-12-30 10:29:30 +0100

    feat: Delete current user

--- 4-natours/routes/userRoutes.js
+++ 4-natours/routes/userRoutes.js
@@ -18,6 +18,7 @@ router.patch(
 );
 
 router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 router
   .route('/')

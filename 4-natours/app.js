d436b25bdf7f550bf2a41b31a339c5bd1c0cac03
shaydeecoder <shaydeecoder@gmail.com>
2022-12-31 10:31:27 +0100

    feat: Implement data sanitization against NoSQL query injection & xss

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +31,12 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against xss
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));

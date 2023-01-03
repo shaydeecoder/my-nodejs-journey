1a1337584e85b3dfeefb0cdca49806bb9564fe4b
shaydeecoder <shaydeecoder@gmail.com>
2023-01-03 10:16:15 +0100

    feat: Populate referenced tour guides

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -157,6 +157,15 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+
+  next();
+});
+
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   // console.log(docs);

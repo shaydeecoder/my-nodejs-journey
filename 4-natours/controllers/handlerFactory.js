6461c99cd55aeeef8d7a1cb5bf0acf4817bb1d6a
shaydeecoder <shaydeecoder@gmail.com>
2023-01-07 12:49:15 +0100

    feat: Improve read performance with indexes

--- 4-natours/controllers/handlerFactory.js
+++ 4-natours/controllers/handlerFactory.js
@@ -78,6 +78,7 @@ exports.getAll = (Model) =>
       .sort()
       .limitFields()
       .paginate();
+    // const doc = await features.query.explain();
     const doc = await features.query;
 
     // Send response
--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -120,6 +120,13 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+// Single field index
+// tourSchema.index({ price: 1 });
+tourSchema.index({ slug: 1 });
+
+// Compound index
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+
 // Creating a virtual property
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;

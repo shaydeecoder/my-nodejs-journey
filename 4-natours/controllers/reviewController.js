3850dcf18dc37ddab75dad551fc41cebfa39f229
shaydeecoder <shaydeecoder@gmail.com>
2023-01-08 07:18:28 +0100

    feat: Calculate average ratings on tour

--- 4-natours/controllers/reviewController.js
+++ 4-natours/controllers/reviewController.js
@@ -6,6 +6,8 @@ exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
+
+  next();
 };
 
 exports.getAllReviews = factory.getAll(Review);
